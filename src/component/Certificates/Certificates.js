13b6f32dfc2ec313716d33892424254468e1c114
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-06 10:43:41 +0530

    some functionality not implimented

--- src/component/Certificates/Certificates.js
+++ src/component/Certificates/Certificates.js
@@ -1,6 +1,34 @@
+import { nanoid } from "nanoid";
+import CertificatesDetails from "./AllCertificates";
+import './certificate.css';
+
 const Certificates=()=>{
     return(
-       <>Certificates</>
+       <div>
+      <div className="certificates">
+        {CertificatesDetails.map((data) => {
+          return (
+            <div key={nanoid()} className="certificate-box">
+              <div className="certificate-title-wrapper">
+                <div className="certificate-icon">{data.icon}</div>
+                <h3 className="certificate-heading">{data.heading}</h3>
+              </div>
+              {data.certificates.map((certificate) => {
+                return (
+                  <div key={nanoid()} className="certificate">
+                    <h4 style={{color:"white",fontSize:"1rem"}}>{certificate.title}</h4>
+                    <p>{certificate.source}</p>
+                    <a href={certificate.img} target="_blank" rel="noreferrer">
+                      <img src={certificate.img} alt="" />
+                    </a>
+                  </div> 
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+       </div>
     );
 }
 export default Certificates;
